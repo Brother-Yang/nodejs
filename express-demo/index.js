2c176ced36ff9236c9b2db5a9ea13f1a38de3455
Brother-Yang <450028105@qq.com>
2023-07-06 17:25:39 +0800

    feat: express

---
+++ express-demo/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const LoginRouter = require('./router/login');
+const HomeRouter = require('./router/home');
+
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// });
+
+app.use('/home', HomeRouter);
+app.use('/login', LoginRouter);
+
+app.listen(3001);
