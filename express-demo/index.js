fa9ba02ad4b572d5d929c3510ca6a93740ac4954
Brother-Yang <450028105@qq.com>
2023-07-10 11:12:46 +0800

    feat: 跨域&静态资源

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const LoginRouter = require('./router/login');
 const HomeRouter = require('./router/home');
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.send('hello world');
--- express-demo/router/home.js
+++ express-demo/router/home.js
@@ -6,10 +6,10 @@ router.get('/', (req, res) => {
   res.send('home');
 });
 
-// router.post('/', (req, res) => {
-//   console.log(req.body, 'body');
-//   res.send({ code: 1 });
-// });
+router.post('/', (req, res) => {
+  console.log(req.body, 'body');
+  res.send({ code: 1 });
+});
 
 router.get('/info/:id', (req, res) => {
   console.log(req.params);
