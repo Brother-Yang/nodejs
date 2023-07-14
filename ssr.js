e80c29cff6cc849690017f3011e6e4bf13e5812a
Brother-Yang <450028105@qq.com>
2023-07-14 14:55:57 +0800

    feat: ejs

---
+++ ssr.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const LoginRouter = require('./router/login');
+const HomeRouter = require('./router/home');
+
+app.use(cors());
+
+app.set('views', './views'); //设置视图的对应目录
+app.set('view engine', 'ejs'); //设置默认的模板引擎
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.use('/home', HomeRouter);
+app.use('/login', LoginRouter);
+
+app.listen(3002);
