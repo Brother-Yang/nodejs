bd56dbf03003300fa022ce0a50522ae1cc6bade1
Brother-Yang <450028105@qq.com>
2023-07-17 09:39:25 +0800

    feat: ejs

--- router/login.js
+++ router/login.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('login');
+  res.render('login', {
+    title: 'hello world',
+    render: `<h2>哈哈哈</h2>`,
+  });
 });
 
 module.exports = router;
