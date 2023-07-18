be945e9c9146835bd1338b9c01baad90d4fc9356
Brother-Yang <450028105@qq.com>
2023-07-18 09:26:14 +0800

    fix: 解析html

--- ssr.js
+++ ssr.js
@@ -9,6 +9,7 @@ app.use(cors());
 
 app.set('views', './views'); //设置视图的对应目录
 app.set('view engine', 'ejs'); //设置默认的模板引擎
+// app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
   res.send('hello world');
