e201040ed0c4f645900300add92b37460e677d97
Brother-Yang <450028105@qq.com>
2023-08-08 09:34:36 +0800

    feat: koa路由和静态文件

--- koa/index.js
+++ koa/index.js
@@ -1,22 +1,38 @@
 const koa = require('koa');
+const Router = require('koa-router');
+const static = require('koa-static');
+const path = require('path');
+
 const app = new koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  console.log(1);
-  await next();
-  console.log(4);
-  ctx.body = 'hello';
+app.use(static(path.join(__dirname, './public')));
+
+// function delay(time) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, time * 1000);
+//   });
+// }
+
+// app.use(async (ctx, next) => {
+//   console.log(1);
+//   await next();
+//   console.log(4);
+//   // ctx.body = 'hello';
+// });
+// app.use(async () => {
+//   console.log(2);
+//   await delay(3);
+//   console.log(3);
+// });
+
+router.post('/list', (ctx) => {
+  ctx.body = ['111', '222', '333'];
 });
-app.use(async () => {
-  console.log(2);
-  await delay(3);
-  console.log(3);
+
+router.get('/list', (ctx) => {
+  ctx.body = [3, 4, 5];
 });
 
-function delay(time) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, time * 1000);
-  });
-}
-
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000);
